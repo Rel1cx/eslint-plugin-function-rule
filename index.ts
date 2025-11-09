5c765f8349b6d7e57edc82de026c6e8b9f880aa3
Rel1cx <rel1cx@proton.me>
2025-11-10 01:40:54 +0800

    Rename defineRuleVisitor to defineRuleListener

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import type { RuleVisitor, RuleDefinition } from "@eslint/core"
+import type { Rule } from "eslint";
 
 const id = 0;
 
-export function defineRuleVisitor(ruleVisitor: RuleVisitor) {
-    const visitor: RuleVisitor = {}
-    for (const key of Object.keys(ruleVisitor)) {
-        visitor[key + `[type!=${id}]`] = ruleVisitor[key]
+export function defineRuleListener(ruleListener: Rule.RuleListener) {
+    const listener: Rule.RuleListener = {}
+    for (const key of Object.keys(ruleListener)) {
+        listener[key + `[type!=${id}]`] = ruleListener[key]
     }
-    return visitor
+    return listener
 }
 
-export default function functionRule(create: RuleDefinition["create"]) {
+export default function functionRule(create: Rule.RuleModule["create"]) {
     return {
         rules: {
             "function-rule": {
